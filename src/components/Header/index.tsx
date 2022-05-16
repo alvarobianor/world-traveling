0002a02de8c6e76dc02252388c66f11fba55726b
Álvaro Medeiros <alvaro.medeiros@hp.com>
2022-05-16 17:44:43 -0300

    header finished

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { Box, Flex } from "@chakra-ui/react";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <Flex flex="1" w="100%" h="100" justifyContent="center" alignItems="center">
+      <Image
+        src="/world-trip-logo.svg"
+        alt="Picture of the author"
+        width={185}
+        height={50}
+      />
+    </Flex>
+  );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Header />
+
       <Text fontSize="6xl" fontWeight="bold">
         Alvaro Bianor Sousa Medeiros
       </Text>
